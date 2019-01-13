b8dc105e1f39585066bcafd6857ff26df0320e1a
rubensigner <signerproducoes@gmail.com>
2019-01-13 17:49:51 -0300

    Adicionado promise na função

--- index.js
+++ index.js
@@ -2,28 +2,17 @@ const log = require('./lib/log')
 
 log.setName('Name 1')
 
-setTimeout(() => {
-  log.setName('Name 2')
-}, 1000)
-
-setTimeout(() => {
-  log.setName('Name 3')
-}, 2000)
-
-setTimeout(() => {
-  log.setName('Name 4')
-}, 3000)
-
-setTimeout(() => {
-  log.setName('Rubens Mesquita')
-}, 4000)
-
-
-console.log(log.getName())
-let time = setInterval(() => {
-  console.log(log.getName())
-  if (log.getName() === 'Rubens Mesquita') {
-    console.log('Fim...')
-    clearInterval(time)
-  }
-}, 1000)
+const arrayUsers = ['Name 1', 'Name 2', 'Name 3', 'Name 4', 'Rubens Mesquita']
+const allPromise = arrayUsers.map((user) => {
+  return new Promise((resolve, reject) => {
+    let wait = setTimeout(() => {
+      log.setName(user)
+      console.log(user)
+      clearTimeout(wait)
+      resolve()
+    }, 1000)
+  })
+})
+Promise.all(allPromise).then(() => {
+  console.log('Fim...')
+})
