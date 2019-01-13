05375f87f9ff32d5dc7162d69694db7bcf10e4aa
rubensigner <signerproducoes@gmail.com>
2019-01-13 16:49:59 -0300

    Adicionado uma função de tempo.

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const log = require('./lib/log')
 
-log.setName('Rubens Mesquita')
+log.setName('Name 1')
 
-console.log('Hello')
+setTimeout(() => {
+  log.setName('Name 2')
+}, 1000)
+
+setTimeout(() => {
+  log.setName('Name 3')
+}, 2000)
+
+setTimeout(() => {
+  log.setName('Name 4')
+}, 3000)
+
+setTimeout(() => {
+  log.setName('Rubens Mesquita')
+}, 4000)
+
+
 console.log(log.getName())
+let time = setInterval(() => {
+  console.log(log.getName())
+  if (log.getName() === 'Rubens Mesquita') {
+    console.log('Fim...')
+    clearInterval(time)
+  }
+}, 1000)
